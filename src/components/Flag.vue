b8bef08442d41d89838ea7166b6d4982efb95791
Bestian Tang 唐宗浩 <besitan@gmail.com>
2018-11-14 15:48:02 +0800

    update fb share button

--- src/components/Flag.vue
+++ src/components/Flag.vue
@@ -4,7 +4,7 @@
       .ui.fluid.card(v-for="(h, index) in hands" v-if="h.id == $route.params.id")
         card(:h="h", :full="true", :book="book", :mySearch="mySearch", @locate="locate")
 
-      iframe(src="https://www.facebook.com/plugins/share_button.php?href=http%3A%2F%2Fwe.alearn.org.tw&layout=button_count&size=large&mobile_iframe=true&appId=485195848253155&width=104&height=28" width="104" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media")
+      iframe(:src="'https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fwe.alearn.org.tw%2F%23%2Fflag%2F' + $route.params.id + '&layout=button_count&size=small&mobile_iframe=true&appId=485195848253155&width=63&height=20'" width="104" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media")
 </template>
 
 <script>
