50f25c1697294865b4a00a5c4588a7919b2a5305
Bestian Tang 唐宗浩 <besitan@gmail.com>
2019-04-15 19:12:58 +0800

    add edit feature on chatbox

--- src/components/Chatbox.vue
+++ src/components/Chatbox.vue
@@ -14,15 +14,23 @@
           | 縮小
     #box
       .ui.list
-        .item(v-for="c in fil(chats).slice(fil(chats).length - 5, fil(chats).length)")
-          router-link(:to="'/flag/'+c.id")
-            img.ui.avatar(:src="c.photoURL || 'http://graph.facebook.com/' + c.id + '/picture'", alt="^_^")
-          a(@click = "key = c.l" v-bind:class = "c.l") [{{c.l}}]
-          vue-markdown
-            | {{c.n}} : {{c.t}}
-          span.gray(v-show="isFull") &nbsp;&nbsp;-
-            vue-markdown 
-              |{{ countDateDiff(c.time) }}
+        .item(v-for="(c, idx) in fil(chats).slice(fil(chats).length - 5, fil(chats).length)" v-bind:key="c.t")
+          .ui(v-show = "edit !== c") 
+              router-link(:to="'/flag/'+c.id")
+                img.ui.avatar(:src="c.photoURL || 'http://graph.facebook.com/' + c.id + '/picture'", alt="^_^")
+              a(@click = "key = c.l" v-bind:class = "c.l") [{{c.l}}]
+              a(@click = "edit = c", v-show="c.id == id")
+                i.edit.icon(title = "edit")
+              vue-markdown
+                | {{c.n}} : {{c.t}}
+              span.gray(v-show="isFull") &nbsp;&nbsp;-
+                vue-markdown 
+                  |{{ countDateDiff(c.time) }}
+          .ui.form(v-show="edit == c")
+            .ui.input
+              input#input(v-model="c.t", placeholder="更新")
+              a.ui.green.small.button(@click="edit = ''; updateChat(c)") 更新
+
         .item.preview(v-if="p.t")
           router-link(:to="'/flag/'+p.id")
             img.ui.avatar(:src="p.photoURL || 'http://graph.facebook.com/' + p.id + '/picture'", alt="^_^")
@@ -73,6 +81,7 @@ export default {
       p: '',
       msg: '',
       key: '',
+      edit: '',
       isFull: false,
       isMini: true,
       label: '閒聊',
@@ -89,10 +98,24 @@ export default {
         n: this.user.providerData[0].displayName,
         t: this.msg,
         l: this.label,
+        edit: false,
         photoURL: this.photoURL || '',
         time: (new Date()).getTime()
       }
       this.p = o
+    },
+    updateChat: function (c) {
+      var o = {
+        id: c.id,
+        n: c.n,
+        t: c.t,
+        l: c.l,
+        photoURL: c.photoURL || '',
+        time: (new Date()).getTime()
+      }
+      chatsRef.child(c['.key']).set(
+        o
+      )
     },
     addChat: function () {
       var o = {
@@ -116,7 +139,9 @@ export default {
     },
     fil: function (list) {
       var k = this.key
-      return list.filter(function (o) { return (o.t + o.l).indexOf(k) > -1 || !k })
+      return list.filter(function (o) { return (o.t + o.l).indexOf(k) > -1 || !k }).map(function (o) {
+        o.edit = false; return o
+      })
     }
   }
 }
@@ -215,6 +240,7 @@ export default {
 /*    bottom: 0; */
     left: 0;
     width: 100%;
+    padding-left: 1em; 
   }
 
   input {
