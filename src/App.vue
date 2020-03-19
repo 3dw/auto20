b42ee3c3e961d62f0dbfd14b39cc212e09af4b6e
Bestian Tang <bestian@gmail.com>
2020-03-19 10:18:46 +0800

    ga event

--- src/App.vue
+++ src/App.vue
@@ -190,6 +190,18 @@ export default {
     if (this.$localStorage.get('book')) {
       this.getLocal('book')
     }
+  },
+  watch: {
+    $route (to, from) {
+      console.log(from.path)
+      console.log(to.path)
+      this.$gtag.event('action', {
+        event_category: 'navigate',
+        event_action: 'from:' + from.path + ' to:' + to.path,
+        event_label: 'from:' + from.path + ' to:' + to.path,
+        value: 'from:' + from.path + ' to:' + to.path
+      })
+    }
   }
 }
 </script>
@@ -259,7 +271,11 @@ body {
 }
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif,
+               'Microsoft JhengHei',
+                'Microsoft YaHei',
+                'STHeiti',
+                'Apple LiGothic Medium';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   width: 100vw;
