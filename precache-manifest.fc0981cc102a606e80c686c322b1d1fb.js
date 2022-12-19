9627c6f4ef5aa6a69603897825f1aec3f71fa2cc
Bestian Tang 大巴 <bestian@gmail.com>
2022-12-19 18:07:16 +0800

    deploy

---
+++ precache-manifest.fc0981cc102a606e80c686c322b1d1fb.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3455d756e0c8284d83834384403fda68",
+    "url": "/ad-be.jpg"
+  },
+  {
+    "revision": "13fb59d9239536817b9d",
+    "url": "/css/app.b47228e6.css"
+  },
+  {
+    "revision": "dd12d4f8d978c8e0f319",
+    "url": "/css/chunk-vendors.8ab2d5cf.css"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/fonts/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/fonts/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/fonts/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/fonts/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/fonts/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/fonts/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/fonts/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/fonts/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/fonts/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/fonts/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/fonts/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/fonts/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/img/brand-icons.a1a749e.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/img/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/img/flags.9c74e17.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/img/flags.9c74e172.png"
+  },
+  {
+    "revision": "f0be4e3ececee3f77b5627664ac39d8f",
+    "url": "/img/handshake0.f0be4e3.png"
+  },
+  {
+    "revision": "f0be4e3ececee3f77b5627664ac39d8f",
+    "url": "/img/handshake0.f0be4e3e.png"
+  },
+  {
+    "revision": "f0be4e3ececee3f77b5627664ac39d8f",
+    "url": "/img/handshake0.png"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/img/icons.962a1bf.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/img/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "9edecf369181ef52cfe523ecfe6cd491",
+    "url": "/img/lead.jpg"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/img/outline-icons.82f60bd.svg"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/img/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "e9499ffd3e0ef1033c0b9c37116df000",
+    "url": "/img/splashscreens/ipad_splash.png"
+  },
+  {
+    "revision": "d7d20618880b92c86f4df30648098eee",
+    "url": "/img/splashscreens/ipadpro1_splash.png"
+  },
+  {
+    "revision": "3a7020f1bdaa5341170b16946b556d10",
+    "url": "/img/splashscreens/ipadpro2_splash.png"
+  },
+  {
+    "revision": "6b7321268eb5f2c5f1752651f2d22e98",
+    "url": "/img/splashscreens/iphone5_splash.png"
+  },
+  {
+    "revision": "e54771d253ef3e89ed870027a7857ae6",
+    "url": "/img/splashscreens/iphone6_splash.png"
+  },
+  {
+    "revision": "684824bb82960ac626dd7aa0a7244e4d",
+    "url": "/img/splashscreens/iphoneplus_splash.png"
+  },
+  {
+    "revision": "49252b2833742dfd0994f3df4f240137",
+    "url": "/img/splashscreens/iphonex_splash.png"
+  },
+  {
+    "revision": "d90e50cf60c1163e8a808a31e4d9564f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13fb59d9239536817b9d",
+    "url": "/js/app.7a085a51.js"
+  },
+  {
+    "revision": "dd12d4f8d978c8e0f319",
+    "url": "/js/chunk-vendors.03c0d86a.js"
+  },
+  {
+    "revision": "eeadb1d7402a05bf6d868b3039be8f8c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,30 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.fc0981cc102a606e80c686c322b1d1fb.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "auto20"});
+
+workbox.core.skipWaiting();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
